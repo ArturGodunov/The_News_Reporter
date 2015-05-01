ce33d1ea0b5057299fa3a6dfc33e3103a8f761e1
Artur Godunov <artur-godunov@mail.ru>
2015-05-01 14:19:32 +0300

    debug main menu

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(window).on("resize", function(){
     moveOut();
 });
 function moveOut(){
+    var navMainItem=$(".nav_main_item");
     if ($(window).width() >= 980) {
         $(".breaking_news_img_wrap").hover(function () {
             $(".move_out_wrap").slideDown(200);
@@ -15,10 +16,12 @@ function moveOut(){
         $(".breaking_news_img_wrap").off("mouseenter mouseleave");
 
         /*vertical menu*/
-        //$(".nav_main_item").hide();
-        //$(".nav_main_list").on("click", function() {
-        //    $(".nav_main_item").toggle();
-        //});
-
+        $(".nav_main_list").on("click", function() {
+            if (navMainItem.hasClass("show")) {
+                navMainItem.removeClass("show");
+            } else {
+                navMainItem.addClass("show");
+            }
+        });
     }
 }
