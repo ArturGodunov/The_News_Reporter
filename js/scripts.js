7003ad5cff4593951e4b52ad89331b6196bc0524
Artur Godunov <artur-godunov@mail.ru>
2015-05-01 13:46:16 +0300

    delete .idea and .sass-cache

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,7 @@ $(window).on("resize", function(){
     moveOut();
 });
 function moveOut(){
-    console.log($(window).width());
     if ($(window).width() >= 980) {
-        $(".nav_main_item").show();
         $(".breaking_news_img_wrap").hover(function () {
             $(".move_out_wrap").slideDown(200);
         }, function () {
@@ -17,9 +15,10 @@ function moveOut(){
         $(".breaking_news_img_wrap").off("mouseenter mouseleave");
 
         /*vertical menu*/
-        $(".nav_main_item").hide();
-        $(".nav_main_list").on("click", function() {
-            $(".nav_main_item").toggle();
-        });
+        //$(".nav_main_item").hide();
+        //$(".nav_main_list").on("click", function() {
+        //    $(".nav_main_item").toggle();
+        //});
+
     }
 }
